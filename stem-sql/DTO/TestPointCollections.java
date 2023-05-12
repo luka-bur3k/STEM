82a91269df35ef168828681823945d49cc9c700f
wachelena <81164555+wachelena@users.noreply.github.com>
2023-05-12 15:11:27 +0200

    Adding files for DTO, some logic and extra
    Also added a "trashed_code" folder, with code that in the end wasn't used, as is explained in READ.ME

---
+++ stem-sql/DTO/TestPointCollections.java
@@ -0,0 +1,12 @@
+public class TestPointCollections {
+    private int id;
+    private int inputConditionID;
+    private int[] sampleIds;
+    private int[] testPoints;
+  
+  
+    public static void main(String[] args) {
+      TestPointCollections testPointCollections = new TestPointCollections();
+      System.out.println(testPointCollections.x);
+    }
+  }
